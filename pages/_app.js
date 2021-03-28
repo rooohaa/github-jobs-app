20afbce22333417c0b55a1fefd52c6decfa29e30
rooohaa <rekebek02@gmail.com>
2021-03-28 14:09:48 +0600

    add/google-fonts

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,19 @@
+import Head from 'next/head'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+   return (
+      <>
+         <Head>
+            <link
+               href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap"
+               rel="stylesheet"
+            />
+            <title>Github Jobs</title>
+         </Head>
+         <Component {...pageProps} />
+      </>
+   )
 }
 
 export default MyApp
